364efa495f1cdab793f7fd2c16e1c1c287baac48
Theodoros Ploumis <me@theodorosploumis.com>
2022-01-14 18:04:06 +0000

    Move terminal config last

--- install.sh
+++ install.sh
@@ -7,13 +7,6 @@ sudo rm -f ~/.gitconfig && cp ~/.dotfiles/.gitconfig ~/.gitconfig
 rm -f ~/.bash_aliases && cp ~/.dotfiles/.bash_aliases ~/.bash_aliases
 chmod +x ~/.bash_aliases
 
-# bashrc file
-# cat ~/.dotfiles/.bashrc >> ~/.bashrc
-rm -f ~/.bashrc && cp ~/.dotfiles/.bashrc ~/.bashrc
-
-# Use my zshrc
-rm -f ~/.zshrc && cp ~/.dotfiles/.zshrc ~/.zshrc
-
 # Install ohmyzsh
 rm -rf ~/.oh-my-zsh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
@@ -23,3 +16,9 @@ rm -rf ~/.scm_breeze && git clone git://github.com/scmbreeze/scm_breeze.git ~/.s
 
 # Install rupa/z
 curl -o ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh && chmod +x ~/z.sh
+
+
+# Custom terminal config should come last
+# cat ~/.dotfiles/.bashrc >> ~/.bashrc
+rm -f ~/.bashrc && cp ~/.dotfiles/.bashrc ~/.bashrc
+rm -f ~/.zshrc && cp ~/.dotfiles/.zshrc ~/.zshrc
